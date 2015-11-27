41d68772c3c024c8e47b58124c6bb708f7f21c09
uas <uas@GCS-5>
2015-11-28 02:58:59 +1030

    hack to pull out data needed to support mavlink msg

--- get_planes.py
+++ get_planes.py
@@ -1,6 +1,7 @@
 __author__ = 'danmir'
 import requests
 import json
+from pymavlink import mavutil
 
 
 def linear_dict_search(inp_dict, input_value):
@@ -100,7 +101,7 @@ class FlightRadarAPI():
     def get_zone_by_coord(self, lon, lat):
         for zone in self.zones:
             print(zone)
-        # TODO: узнать url зон.
+            #TODO make this work?
 
     def _get_zone(self, zone, start_zone):
         pass
@@ -118,10 +119,17 @@ class FlightRadarAPI():
         :return: list
         """
         # Expand zone a little
+        """
         lat_ne = int(lat_ne) + 1
         lat_sw = int(lat_sw) - 1
         lng_ne = int(lng_ne) + 1
         lng_sw = int(lng_sw) - 1
+        """
+        lat_ne = int(lat_ne)
+        lat_sw = int(lat_sw)
+        lng_ne = int(lng_ne)
+        lng_sw = int(lng_sw)
+        
         zone_link = "http://{}/zones/fcgi/feed.js?bounds={},{},{},{}&adsb=1&mlat=1&flarm=1&faa=1&estimated=1&air=1&gnd=1&vehicles=1&gliders=1&array=1".format(self.server_data_link, lat_ne, lat_sw, lng_sw, lng_ne)
         # print(zone_link)
         info = self.parce_json(zone_link)
@@ -148,17 +156,64 @@ class FlightRadarAPI():
         info = self.parce_json(aircraft_link)
         return info
 
-
+def format_aircraft_data(aircrafts):
+    
+    for aircraft in aircrafts:
+        print aircraft
+        unsure = aircraft[0] #hash for that flight?
+        ICAO_address = aircraft[1] #hex reg, unique
+        lat = aircraft[2]*1e7
+        lon = aircraft[3]*1e7
+        heading = aircraft[4]
+        altitude = aircraft[5]*0.3048 #ft to m
+        hor_velocity = aircraft[6]*0.514444 #knots to m/s
+        squawk = aircraft[7]
+        id_radar = aircraft[8] #id picking up the plane, where data comes from
+        aircraft_type = aircraft[9]
+        callsign = str(aircraft[10]) #unique reg (not hex)
+        data_get_time = aircraft[11] #utc?
+        takeoff_airport = aircraft[12]
+        dest_airport = aircraft[13]
+        flight_name_takeoff = aircraft[14]
+        on_ground_flag = aircraft[15] #0 = air, 1 = ground
+        ver_velocity=aircraft[16]*0.00508 #fpm to m/s
+        flight_name_dest = aircraft[17]
+        unsure = aircraft[18]
+    print ""
+    
+    """
+     <message id="246" name="ADSB_VEHICLE">
+          <description>The location and information of an ADSB vehicle</description>
+          <field type="uint32_t" name="ICAO_address">ICAO address</field>
+          <field type="int32_t" name="lat">Latitude, expressed as degrees * 1E7</field>
+          <field type="int32_t" name="lon">Longitude, expressed as degrees * 1E7</field>
+          <field type="uint8_t" name="altitude_type" enum="ADSB_ALTITUDE_TYPE">Type from ADSB_ALTITUDE_TYPE enum</field>
+          <field type="float" name="altitude">Altitude(ASL) in meters</field>
+          <field type="uint16_t" name="heading">Course over ground in centidegrees</field>
+          <field type="float" name="hor_velocity">The horizontal velocity in meters/second</field>
+          <field type="float" name="ver_velocity">The vertical velocity in meters/second, positive is up</field>
+          <field type="char[9]" name="callsign">The callsign, 8+null</field>
+          <field type="uint8_t" name="emitter_type" enum="ADSB_EMITTER_TYPE">Type from ADSB_EMITTER_TYPE enum</field>
+          <field type="uint8_t" name="tslc">Time since last communication in seconds</field>
+          <field type="uint16_t" name="flags" enum="ADSB_FLAGS">Flags to indicate various statuses including valid data fields</field>
+        </message>
+    """
 
 if __name__ == "__main__":
+    
+    import time
     flapi = FlightRadarAPI()
     print(flapi.server_data_link)
-    print(flapi.airports)
-    print(flapi.airlines)
-    print(flapi.zones)
-    print(flapi.get_aircrafts_by_zone("atlantic"))
-    print(flapi.get_aircraft_info("50e10b0"))
-    print(flapi.get_aircrafts_by_bounds(36, 31, -121, -117))
-    # print(flapi.get_zone_by_coord(60.599637, 56.834280))
-    # print(flapi.get_aircrafts())
-    print(flapi.get_aircrafts_by_flight_num("SU1501"))
+#     print(flapi.airports)
+#     print(flapi.airlines)
+#     print(flapi.zones)
+#     print(flapi.get_aircrafts_by_zone("atlantic"))
+#     print(flapi.get_aircraft_info("50e10b0"))
+#
+
+    while True:
+        rep = (flapi.get_aircrafts_by_bounds(-29, -40, 128, 143))
+        format_aircraft_data(rep)
+        
+#     print(flapi.get_zone_by_coord(60.599637, 56.834280))
+#     print(flapi.get_aircrafts_by_flight_num("SU1501"))
